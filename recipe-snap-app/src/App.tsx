69789fdd74b3a96cb39c9534510e48b124ff7c56
raven18 <kaur9720@sheridancollege.ca>
2023-05-06 19:24:08 -0400

    Init

---
+++ recipe-snap-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h5>Hello From React App</h5>
+      </header>
+    </div>
+  );
+}
+
+export default App;
